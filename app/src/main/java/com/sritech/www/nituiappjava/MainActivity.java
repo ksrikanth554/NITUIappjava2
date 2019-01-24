951a2bcea1e287f91dda2bf818615a3b282f092d
sritechsoftsolutions <kotlasrikanth777@gmail.com>
2019-01-24 17:13:27 +0530

    updated uiapp in java

--- app/src/main/java/com/sritech/www/nituiappjava/MainActivity.java
+++ app/src/main/java/com/sritech/www/nituiappjava/MainActivity.java
@@ -3,15 +3,42 @@ package com.sritech.www.nituiappjava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button b1;
+    private EditText e;
+    private TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e=findViewById(R.id.edt1);
+        t=findViewById(R.id.txt1);
+        b1=findViewById(R.id.b1);
+
+        //Anonymous InnerClass
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s=e.getText().toString();
+                t.setText(s);
+                e.setText("");
+
+            }
+        });
+
+       //Lamda Expression
+       /* b1.setOnClickListener(v -> {
+            String s=e.getText().toString();
+            t.setText(s);
+            e.setText("");
+
+        });*/
+
+      // b1.setOnClickListener(this);
     }
    public void getText(View view)
    {
@@ -23,4 +50,11 @@ public class MainActivity extends AppCompatActivity {
 
 
    }
+
+   /* @Override
+    public void onClick(View v) {
+       String s=e.getText().toString();
+       t.setText(s);
+       e.setText("");
+    }*/
 }
